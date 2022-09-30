70fad17d65cfde780aab672031631649645f8039
hollymsnell <hollymsnell@gmail.com>
2022-09-30 16:25:37 -0600

    create function working

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ export default {
   data: function () {
     return {
       recipes: [],
+      newRecipeParams: {},
     };
   },
   created: function () {
@@ -16,12 +17,54 @@ export default {
         this.recipes = response.data;
       });
     },
+    createRecipe: function () {
+      axios
+        .post("/recipes", this.newRecipeParams)
+        .then((response) => {
+          console.log("recipes create", response);
+          this.recipes.push(response.data);
+          this.newRecipeParams = {};
+        })
+        .catch((error) => {
+          console.log("recipes create error", error.response);
+        });
+    },
   },
 };
 </script>
 
 <template>
   <div class="home">
+    <h1>New Recipe</h1>
+    <div>
+      <div>
+        Name:
+        <input type="text" v-model="newRecipeParams.name" />
+      </div>
+      <div>
+        Ingredients:
+        <input type="text" v-model="newRecipeParams.ingredients" />
+      </div>
+      <div>
+        Instructions:
+        <input type="text" v-model="newRecipeParams.instructions" />
+      </div>
+      <div>
+        Prep-minutes:
+        <input type="text" v-model="newRecipeParams.prep_minutes" />
+      </div>
+      <div>
+        Cook-minutes:
+        <input type="text" v-model="newRecipeParams.cook_minutes" />
+      </div>
+      <div>
+        Image:
+        <input type="text" v-model="newRecipeParams.image" />
+      </div>
+      <div>
+        <button v-on:click="createRecipe()">Create Recipe</button>
+      </div>
+    </div>
     <h1>All Recipes</h1>
     <div v-for="recipe in recipes" v-bind:key="recipe.id">
       <h2>{{ recipe.name }}</h2>
