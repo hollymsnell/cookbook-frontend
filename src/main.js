47e4794b40dcc659fa1ae1ade3e7412c76823a4f
hollymsnell <hollymsnell@gmail.com>
2022-12-26 14:11:29 -0700

    configure web request to backend and add prettier rule

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "http://billowing-breeze-5166.fly.dev";
 
 createApp(App).use(router).mount("#app");
