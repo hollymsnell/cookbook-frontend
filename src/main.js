4dc191041a059d5e3ac0d0d26bb213b784ad170b
hollymsnell <hollymsnell@gmail.com>
2022-12-26 14:21:36 -0700

    fix web request

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "http://billowing-breeze-5166.fly.dev";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://billowing-breeze-5166.fly.dev";
 
 createApp(App).use(router).mount("#app");
